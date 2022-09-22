b6d637c6ded76d81e77fd1dfbde12f5982ca52dc
kenechvkwv <ubesiekene@gmail.com>
2022-09-22 11:33:21 +0100

    app3

---
+++ randomJs/app3.js
@@ -0,0 +1,17 @@
+//If and else statement. for checkout value, check app.js
+
+if ((checkout = false)) {
+  console.log("no money");
+} else {
+  console.log("you're good to go");
+}
+
+//another example
+var age = 20;
+if (age > 21) {
+  console.log("You are old enough");
+} else if ((age === 17, 18, 19, 20)) {
+  console.log("tss, close, try again in a few years");
+} else {
+  console.log("you really should not be here");
+}
