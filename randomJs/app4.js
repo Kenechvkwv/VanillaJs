2832fe9886e5e4a6227d35911fa55c41c2fb60fe
kenechvkwv <ubesiekene@gmail.com>
2022-09-22 15:32:42 +0100

    app4

---
+++ randomJs/app4.js
@@ -0,0 +1,18 @@
+//Arrays
+const schedule = ["hello", "eat", "workout", "watch a video"];
+console.log(schedule);
+
+//add content to array
+// schedule.push("new content");
+// console.log(schedule);
+
+//add content to front of the array
+schedule.unshift("new content");
+console.log(schedule);
+
+// remove content from array
+// schedule.pop();
+// console.log(schedule);
+
+var film = schedule.indexOf("watch a video");
+console.log(film);
