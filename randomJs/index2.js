a287218936b0c6a317363ca1ceee9905d509f0f3
kenechvkwv <ubesiekene@gmail.com>
2022-10-01 10:12:37 +0100

    more teachings

---
+++ randomJs/index2.js
@@ -0,0 +1,21 @@
+//Using the console.log
+console.log("good morning");
+console.log("mini-me");
+
+//variables
+var car = "lexus";
+let bike = "mitsubishi";
+const man = "Obi";
+
+//Testing variable usage
+console.log(car);
+
+car = "benz";
+
+console.log(car);
+
+console.log(bike);
+bike = "PCJ 600";
+console.log(bike);
+
+console.log(man);
